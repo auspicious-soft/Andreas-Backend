9bd6ec697175e43485971e475680c7e83964b7da
Auspicious <info@auspicioussoft.com>
2024-12-18 15:19:43 +0530

    Remove payment validation schema

--- src/validation/payment.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const buyPlanSchema = z.object({
-    planType: z.enum(['free', 'intro', 'pro']),
-    interval: z.enum(['month', 'year'])
-}).strict({
-    message: "Bad payload present in the data"
-})
