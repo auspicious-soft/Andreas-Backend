a94f01ed4beeb69e52f70f07390c1f0d141562b7
Auspicious <info@auspicioussoft.com>
2025-01-10 15:28:34 +0530

    Update projects schema to make fields optional and improve formatting

--- src/models/user/projects-schema.ts
+++ src/models/user/projects-schema.ts
@@ -1,17 +1,17 @@
 import { Schema, model } from "mongoose";
 
 const projectsSchema = new Schema({
-    identifier: {type: String,unique: true},
+    identifier: { type: String, unique: true },
     projectName: { type: String, required: true },
     userId: { type: Schema.Types.ObjectId, required: true, ref: "users" },
-    projectimageLink: { type: String, required: true },
-    projectstartDate: { type: String, required: true },
-    projectendDate: { type: String, required: true },
-    description: { type: String, required: true },
+    projectimageLink: { type: String, required: false },
+    projectstartDate: { type: String, required: false },
+    projectendDate: { type: String, required: false },
+    description: { type: String, required: false },
     associates: { type: [String], required: false },
     status: { type: String, required: false },
     progress: { type: Number, required: false, default: 0 },
-    employeeId: { type: Schema.Types.ObjectId, required: true, ref: "employees" },
+    employeeId: { type: Schema.Types.ObjectId, required: false, ref: "employees" },
     createdby: { type: String, required: true },
 }, { timestamps: true })
 
