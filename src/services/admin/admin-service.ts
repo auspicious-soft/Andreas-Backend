f29a7b3da730997055f06057c83e3fd1373dbdec
Auspicious <info@auspicioussoft.com>
2025-01-14 17:08:18 +0530

    sd

--- src/services/admin/admin-service.ts
+++ src/services/admin/admin-service.ts
@@ -273,10 +273,10 @@ export const getDashboardStatsService = async (payload: any, res: Response) => {
 
     const ongoingProjectCount = await projectsModel.countDocuments({ progress: { $ne: 100 } })
     const completedProjectCount = await projectsModel.countDocuments({ progress: 100 })
-    const workingProjectDetails = await projectsModel.find({ progress: { $ne: 100 } }).select("projectName projectimageLink projectstartDate projectendDate status identifier"); // Adjust the fields as needed
+    const workingProjectDetails = await projectsModel.find({ progress: { $ne: 100 } }).select("projectName projectimageLink projectstartDate projectendDate status identifier progress"); // Adjust the fields as needed
 
     const sevenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 7))
-    const recentProjectDetails = await projectsModel.find({ createdAt: { $gte: sevenDaysAgo } }).select("projectName projectimageLink projectstartDate projectendDate identifier"); // Adjust the fields as needed
+    const recentProjectDetails = await projectsModel.find({ createdAt: { $gte: sevenDaysAgo } }).select("projectName projectimageLink projectstartDate projectendDate identifier progress"); // Adjust the fields as needed
 
     const response = {
         success: true,
